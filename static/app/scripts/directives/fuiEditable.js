7037720ca49f6293b60c4933186c6558a262e845
Pearson <pearson@INAIRDT541313.(none)>
2014-01-28 16:50:15 +0530

    FS-3329: Worked on feedback provided

--- static/app/scripts/directives/fuiEditable.js
+++ static/app/scripts/directives/fuiEditable.js
@@ -1,26 +1,57 @@
+
 'use strict';
 
 angular.module('retrospectApp')
+    .controller('Ctrl', [
+        'retrospectives'
+    ])
     .directive('fuiEditable', function () {
-    return {
-        templateUrl: 'views/directives/fuiEditable.html',
-        restrict: 'EA',
-        replace: true,
-        scope: {
-            item: '=',
-            onDelete: '&',
-            onUpdate: '&'
+        return {
+            templateUrl: 'views/directives/fuiEditable.html',
+            restrict: 'EA',
+            replace: true,
+            scope: {
+                item: '=',
+                onDelete: '&',
+                onUpdate: '&'
             },
-        transclude: true,
-        link: function postLink(scope, element, attrs) {
-            scope.toggleEditmode = function() {
-                scope.showEdit = !scope.showEdit;
-            }
+            transclude: true,
+            link: function postLink(scope, element, attrs) {
+                scope.toggleEditmode = function() {
+                    scope.showEdit = !scope.showEdit;
+                };
 
-            scope.saveChange = function () {
-                scope.onUpdate();
-                scope.toggleEditmode();
+                scope.saveChange = function () {
+                    scope.onUpdate();
+                    scope.toggleEditmode();
+                };
             }
-        }
-    };
+        };
+    })
+    .directive('fuiRetroname', function (retrospectives) {
+        return {
+            templateUrl: 'views/directives/fuiRetroname.html',
+            restrict: 'EA',
+            replace: true,
+            scope: {
+                item: '=',
+                onUpdate: '&'
+            },
+            transclude: true,
+            link: function postLink(scope, element, attrs) {
+                scope.toggleEditmode = function() {
+                    scope.showEdit = !scope.showEdit;
+                };
+
+                scope.updateRetro = function () {
+                    retrospectives.update({'retroId': scope.item.id}, {'name':scope.item.name}, function () {
+                        console.log('Updating...');
+                    }, function (error) {
+                        console.error('Error updating retrospective name', error);
+                    });
+                    console.log('Updating.......');
+                    scope.toggleEditmode();
+                };
+            }
+        };
     });
